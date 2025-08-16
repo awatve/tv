648a6fd3c32c3689be5e7d3f9a8204689346aa7a
awatve <watve.ahaan@gmail.com>
2025-08-16 12:30:26 -0400

    update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 const { writeFileSync } = require("fs");
 
-const SOURCE = "https://freelivtv.xyz/watchindia/gudu.php?id=63544";
+const SOURCE = "https://tataplay.slivcdn.com/hls/live/2011749/SABHD/master_3500.m3u8";
 const PROXY  = "https://cors-proxy.cooks.fyi/";
 
 async function updateM3U8() {
@@ -17,8 +17,8 @@ async function updateM3U8() {
       return PROXY + new URL(line, SOURCE).toString();
     });
 
-    writeFileSync("star_pravah_hd.m3u8", lines.join("\n"));
-    console.log("✅ star_pravah_hd.m3u8 updated successfully!");
+    writeFileSync("sony_sab_hd.m3u8", lines.join("\n"));
+    console.log("Sony SAB HD");
   } catch (err) {
     console.error("❌ Error updating playlist:", err);
     process.exit(1);
